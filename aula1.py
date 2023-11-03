a3aab32f20b62c4bb94562e92b0290cea0def4f6
eaejaum <jpedroalmeida179@gmail.com>
2023-11-03 12:19:07 -0300

    Aula2

---
+++ aula1.py
@@ -0,0 +1,10 @@
+"""
+DocString (Não é um comentário)
+Mais de uma linha de código
+"""
+'''
+Comenta mais de uma linha de código
+'''
+# Comenta uma linha de código (Único que de fato é um comentário em Python)
+print(123)
+print(456)
---
+++ aula2.py
@@ -0,0 +1,7 @@
+# Função "print" Argumento "()" => print()
+# sep="" = Separador
+
+
+print(12, 34, sep='-')
+print(56, 78, sep="-")
+print(9, 10, sep="-")
