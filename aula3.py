caadb0f37a0c1a8f4aef01c1d89f1ad0bf4b6f20
eaejaum <jpedroalmeida179@gmail.com>
2023-11-06 09:13:32 -0300

    Aula 3

---
+++ aula3.py
@@ -0,0 +1,24 @@
+"""
+Python = Linguagem de programação
+Tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentrp de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('João Pedro')
+print(1, 'João "Pedro"')
+
+
+# Aspas duplas
+print("João Pedro")
+print(2, "João 'Pedro'")
+
+
+# Escape
+print("João \"Pedro\"")
+
+# r
+print(r"João \"Pedro\"")
