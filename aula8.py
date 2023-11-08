c84d9e90a4ab8c77d75a6a36024fd139a68b117a
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 08:52:22 -0300

    Primeiro Exercício

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'João Pedro'
+sobrenome = 'Santos de Almeida'
+idade = 18
+ano_nascimento = 2005
+maior_de_idade = idade >= 18
+altura = 1.75
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?:', maior_de_idade)
+print('Altura em metros:', altura)
