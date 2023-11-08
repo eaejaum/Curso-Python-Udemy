791d3178fc72b17e18c3b8ec909e5b8b993c475c
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 08:56:09 -0300

    Introdução às Variáveis

--- aula7.py
+++ aula7.py
@@ -6,7 +6,7 @@ PEP8: inicie variáveis com letras minúsculas, pode usar
 números e underline _.
 O sinal de = é o operador de atribuição. Ele é usado para
 atribuir um valor a um nome (variável).
-Uso: nome_variavel = expressão
+Uso: nome_variavel = expressão 
 '''
 
 nome_completo = 'João Pedro Santos de Almeida'
