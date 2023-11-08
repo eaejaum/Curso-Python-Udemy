b055fd4fce1ed713b896d91fafee29bc86ab290d
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 11:47:32 -0300

    Função input

---
+++ aula15.py
@@ -0,0 +1,12 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_1 = int(numero_1)
+int_2 = int(numero_2)
+
+soma = int_1 + int_2
+
+print(f'A soma dos números é : {soma}')
