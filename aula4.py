d490efc5c93533770efeb13decbfd066011076a2
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 08:10:06 -0300

    Aula 4

---
+++ aula4.py
@@ -0,0 +1,29 @@
+# TIPOS DE VARIÁVEIS INT E FLOAT 
+
+# int -> Número inteiro
+'''
+O tipo int representa qualquer número positivo ou negativo. 
+int sem sinal ( - ), é considerado positivo.
+'''
+print("Números inteiros")
+print(11)
+print(-11)
+print(0)
+
+# float ->  Número com ponto flutuante
+'''
+O tipo float representa qualquer número 
+positivo ou negativo com ponto flutante. 
+float sem sinal é considerado positivo.
+'''
+print("Números com ponto flutuante")
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+'''
+A função type mostra o tipo que o Python inferiu ao valor.
+'''
+
+print(   type('Otávio')   ) 
+print(   type(12)   ) 
+print(   type(1.200)   ) 
