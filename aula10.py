521eabbe44128c947d60b6d247b4300615cfc13b
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 10:41:19 -0300

    Concatenação (+) e repetição (*) com operadores

---
+++ aula10.py
@@ -0,0 +1,9 @@
+# Concatenação (+) e repetição (*) com operadores aritméticos
+
+concatenacao = 'João' + ' ' + 'Pedro'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+joao_tres_vezes = 'João' * 3
+print(joao_tres_vezes)
