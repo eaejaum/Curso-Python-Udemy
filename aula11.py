4d77e04ddbec01403bd62d6b2c3987b2588f5a4e
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 10:49:01 -0300

    Precedência entre os operadores aritméticos

---
+++ aula11.py
@@ -0,0 +1,13 @@
+# Precedência entre os operadores aritméticos
+
+'''
+Ordem 
+
+1° parênteses
+2° exponenciação
+3° multiplicação, divisão, divisão inteira, módulo
+4° adição, subtração
+'''
+
+conta = (1 + 1) ** (5 + 5) // 2
+print(conta)
