b0a4fab6a1479a2de9d8dcfd6d1aae2bf8ec1f0b
eaejaum <jpedroalmeida179@gmail.com>
2023-11-08 09:02:40 -0300

    DocStrings e Comentários

--- aula1.py
+++ aula1.py
@@ -6,5 +6,5 @@ Mais de uma linha de código
 Comenta mais de uma linha de código
 '''
 # Comenta uma linha de código (Único que de fato é um comentário em Python)
-print(123)
-print(456)
+print(1234)
+print(5678)
