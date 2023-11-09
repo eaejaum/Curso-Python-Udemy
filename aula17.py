369477a5b8d39b5ab5338518ea4327e377b8d62d
eaejaum <jpedroalmeida179@gmail.com>
2023-11-09 09:45:05 -0300

    Introdução if/elif/else - pt2

---
+++ aula17.py
@@ -0,0 +1,39 @@
+# if / elif      / else
+# se / se não se / se não 
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+#EXECUTA APENAS UMA CONDIÇÃO DAS VERDADEIRAS
+
+# if condicao1: 
+#     print('Código para condição 1')
+# elif condicao2:
+#     print('Código para condição 2')
+# elif condicao3:
+#     print('Código para condição 3')
+# elif condicao4:
+#     print('Código para condição 4')
+# else: 
+#     print('Nenhuma condição foi satisfeita')
+
+#EXECUTA TODAS AS CONDIÇÕES QUE ESTIVEREM VERDADEIRAS
+
+# if condicao1: 
+#     print('Código para condição 1')
+# else: 
+#     print('Nenhuma condição foi satisfeita')
+# if condicao2:
+#     print('Código para condição 2')
+# else: 
+#     print('Nenhuma condição foi satisfeita')
+# if condicao3:
+#     print('Código para condição 3')
+# else: 
+#     print('Nenhuma condição foi satisfeita')
+# if condicao4:
+#     print('Código para condição 4')
+# else: 
+#     print('Nenhuma condição foi satisfeita')
