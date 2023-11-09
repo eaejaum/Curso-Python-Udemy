d2b4ac7ec09465ed5679ddb11e7a44f03c479308
eaejaum <jpedroalmeida179@gmail.com>
2023-11-09 10:55:32 -0300

    Exercício Comparador de Números

---
+++ exComparador.py
@@ -0,0 +1,11 @@
+
+num_1 = int(input('Digite o primeiro número: '))
+num_2 = int(input('Digite o segundo número: '))
+
+if num_1 > num_2:
+    print('O primeiro número é maior que o se segundo número')
+elif num_1 < num_2:
+    print('O primeiro número é menor que o segundo número')
+elif num_1 == num_2:
+    print('Os números são iguais')
+
