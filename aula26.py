2404ea70eb968e55223063d3f5da156d3a75be78
eaejaum <jpedroalmeida179@gmail.com>
2023-11-10 11:19:01 -0300

    Fatiamento de Strings

---
+++ aula26.py
@@ -0,0 +1,13 @@
+"""
+Fatiamento de strings
+
+    012345678
+    Olá mundo
+   -987654321 Negativos
+
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna qtd de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+print(variavel[::-1])
