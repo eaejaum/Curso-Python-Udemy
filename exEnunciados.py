fc622cbfd418c82812c0656f44111b5dfb045b0f
eaejaum <jpedroalmeida179@gmail.com>
2023-11-14 09:36:32 -0300

    Exercício Par/Ímpar,  Hora Atual,  Tamanho do Nome

---
+++ exEnunciados.py
@@ -0,0 +1,47 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+# entrada = input('Digite um número: ')
+
+# if entrada.isdigit(): # verifica se o número é inteiro
+#     numero = int(entrada)
+#     if numero % 2 == 0:
+#         print('Seu número é par')    
+#     else:
+#         print('Seu número é ímpar')
+# else:
+#     print('Digite um número inteiro!')
+    
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+# hora = int(input('Digite a hora atual: '))
+
+# if hora <= 12:
+#     print(f'São {hora} horas. Bom dia!')
+# elif hora > 12 and hora <= 18:
+#     print(f'São {hora} horas. Boa tarde!')
+# elif hora > 18 and hora <= 23:
+#     print(f'São {hora} horas. Boa noite!')
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+
+# nome = input('Digite seu primeiro nome: ')
+# # tamanho = len(nome)
+
+# if len(nome) <= 4:
+#     print('Seu nome é curto!')
+# elif len(nome) >= 5 and len(nome) <= 6:
+#     print('Seu nome é normal!')
+# elif len(nome) > 6:
+#     print('Seu nome é muito grande!')
