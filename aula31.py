aec1599bddc92a7730e7e6e59fb9c639538584bc
eaejaum <jpedroalmeida179@gmail.com>
2023-11-14 12:05:20 -0300

    While, Break, Continue

--- aula31.py
+++ aula31.py
@@ -7,8 +7,18 @@ Loop infinito -> Quando um código não tem fim
 
 contador = 0
 
-while contador <= 10:
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6')
+        continue
+    
     print(contador)
-    contador = contador + 1
+
+    if contador == 40:
+        break
+
+
 
 print('Acabou') 
