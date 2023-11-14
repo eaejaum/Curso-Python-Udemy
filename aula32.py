d383baa897e5c73b2d973f8613af3f7e57cb5c42
eaejaum <jpedroalmeida179@gmail.com>
2023-11-14 11:43:57 -0300

    Operadores de Atribuição

---
+++ aula32.py
@@ -0,0 +1,33 @@
+'''
+Operadores de atribuição
+=  +=  -=  *=  /=  //=  **=  %=
+'''
+
+mais = 10
+menos = 10
+multiplicacao = 10
+divisao_simples = 10
+divisao_dupla = 10
+potencia = 10
+modulo = 10
+
+mais += 1 
+print(mais) # 11
+
+menos -= 1
+print(menos) # 9
+
+multiplicacao *= 2
+print(multiplicacao) # 20
+
+divisao_simples /= 2
+print(divisao_simples) # 5.0
+
+divisao_dupla //= 2
+print(divisao_dupla) # 5
+
+potencia **= 2
+print(potencia) # 10000
+
+modulo %= 2
+print(modulo) # 0 
