24caef8922d9002b83b1c0c5ef7e87dc914e0796
eaejaum <jpedroalmeida179@gmail.com>
2023-11-14 08:28:54 -0300

    Flags: is, is not e None

--- aula30.py
+++ aula30.py
@@ -5,9 +5,16 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 '''
 
-condicao = True
+condicao = False
+passou_no_if = None
 
 if condicao:
+    passou_no_if = True
     print('Faça algo')
 else:
     print('Não faça algo')
+
+if passou_no_if is not None:
+    print('Passou no if')
+else:
+    print('Não passou no if')
