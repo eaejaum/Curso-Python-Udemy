8fb7d01c81674c3c4fcfb213d0388acd5a757d62
eaejaum <jpedroalmeida179@gmail.com>
2023-11-16 09:31:58 -0300

    Exercício While

---
+++ exWhile.py
@@ -0,0 +1,12 @@
+nome = "João Pedro"
+tamanho_nome = len(nome)
+indice = 0
+novo_nome = ''
+
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
