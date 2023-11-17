1a5c5395f21112879bc106f3bb0766a725904b78
eaejaum <jpedroalmeida179@gmail.com>
2023-11-17 09:24:38 -0300

    For / Range

--- aula34.py
+++ aula34.py
@@ -2,8 +2,19 @@
 For + Range
 range -> range(start, stop, step)
 '''
-numeros = range(0, 100, 2)
+# numeros = range(0, 100, 2)
 
-for numero in numeros:
-    print(numero)
+# for numero in numeros:
+#     print(numero)
 
+for i in range(10):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue
+    # if i == 8:
+    #     print('i é 8, seu else não executará')
+    #     break
+    for j in range(1, 3):
+        print(i, j)
+else:
+    print('For completo com sucesso!')
