a24c550a852a263f255ac1681c4a687f6934fff5
eaejaum <jpedroalmeida179@gmail.com>
2023-11-21 12:19:34 -0300

    Listas

---
+++ aula35.py
@@ -0,0 +1,24 @@
+'''
+Listas em Python
+TIpo list - Mutável
+Suporta vários valores de qualquer tipo 
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: 
+    append, insert, pop, del, clear, extend, +
+Create, Read, Update, Delete
+'''
+
+#       +01234
+#       -54321
+
+string = 'ABCDE' # 5 caracteres (len)
+# print(bool([])) # falsy
+# print(lista, type(lista))
+
+#         0     1        2         3   4
+#        -5    -4       -3        -2  -1
+lista = [123, True, 'João Pedro', 1.2, []]
+print(lista)
+lista[-3] = 'Maria'
+print(lista)
+print(lista[2], type(lista[2]))
